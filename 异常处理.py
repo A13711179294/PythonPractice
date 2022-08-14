cbdbf68db31dbf15cc35f4076db4fbe64a32852e
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2022-08-14 17:55:08 +0800

    Add files via upload

---
+++ 异常处理.py
@@ -0,0 +1,18 @@
+try:
+    # print(b)
+    #print(10/0)
+    li=[12,453,216]
+    print(li[10])
+    pass
+# except NameError as msg:
+#     print(msg)
+#     pass
+# except IndexError as msg:
+#     print(msg)
+#     pass
+#except ZeroDivisionError as msg:
+#     print(msg)
+#     pass
+except Exception as result:
+    print(result)
+print(12345)
