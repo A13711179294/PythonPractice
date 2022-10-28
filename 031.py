9851fc7d7047f3fb503172b46fb4f21382945ae9
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2022-10-29 00:50:17 +0800

    Add files via upload

---
+++ 031.py
@@ -0,0 +1,16 @@
+#请输入星期几的第一个字母来判断一下是星期几，如果第一个字母一样，则继续判断第二个字母。
+
+weekT={'h':'thursday',
+       'u':'tuesday'}
+weekS={'a':'saturday',
+       'u':'sunday'}
+week={'t':weekT,
+      's':weekS,
+      'm':'monday',
+      'w':'wensday',
+      'f':'friday'}
+a=week[str(input('请输入第一位字母:')).lower()]
+if a==weekT or a==weekS:
+    print(a[str(input('请输入第二位字母:')).lower()])
+else:
+    print(a)
