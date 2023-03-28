2c4b2a61a4612cb62564ee5ca8e5cfad523ae0bc
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2023-03-28 10:00:27 +0800

    Add files via upload

---
+++ P1435.py
@@ -0,0 +1,15 @@
+s1 = list(input())
+s2 = list(reversed(s1))
+l = len(s1)
+s1.insert(0, 0)
+s2.insert(0, 0)
+
+dp = [[0] * (l + 1) for _ in range(l + 1)]
+for i in range(1, l + 1):
+    for j in range(1, l + 1):
+        if s1[i] == s2[j]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+print(l - dp[l][l])
