da614f8d520b1527f3b3890872ad5423224571f7
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2023-07-03 01:09:17 +0800

    Add files via upload

---
+++ P2433.py
@@ -0,0 +1,39 @@
+from math import sqrt
+
+T = int(input())
+pi = 3.141593
+if T == 1:
+    print('I love Luogu!')
+elif T == 2:
+    print(6, 4)
+elif T == 3:
+    print(3)
+    print(12)
+    print(2)
+elif T == 4:
+    print('%.6g' % (500 / 3))
+elif T == 5:
+    print(int((260 + 220) // (20 + 12)))
+elif T == 6:
+    print('10.8167')
+elif T == 7:
+    print(100 + 10)
+    print(100 + 10 - 20)
+    print(0)
+elif T == 8:
+    print(31.4159)
+    print(78.5398)
+    print(523.599)
+elif T == 9:
+    print(22)
+elif T == 10:
+    print(9)
+elif T == 11:
+    print('33.3333')
+elif T == 12:
+    print(13)
+    print('R')
+elif T == 13:
+    print(16)
+elif T == 14:
+    print(50)
