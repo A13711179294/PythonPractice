5bfcd207cb093390b9fcff945f573f4efcc24aeb
A13711179294 <100777743+A13711179294@users.noreply.github.com>
2023-08-12 14:37:14 +0800

    Add files via upload

---
+++ P3719.py
@@ -0,0 +1,30 @@
+from sys import setrecursionlimit
+setrecursionlimit(100000)
+
+
+def dfs():
+    global pos, length
+    ans, temp = 0, 0
+    while pos < length:
+        if s[pos] == '(':
+            pos += 1
+            temp += dfs()
+        elif s[pos] == 'a':
+            pos += 1
+            temp += 1
+        elif s[pos] == '|':
+            pos += 1
+            ans = max(ans, temp)
+            temp = 0
+        elif s[pos] == ')':
+            pos += 1
+            break
+
+    ans = max(ans, temp)
+    return ans
+
+
+s = input().strip()
+pos, length = 0, len(s)
+ret = dfs()
+print(ret)
